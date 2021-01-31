20d17163da731337fd3f3676cf0c2e1569adf667
igithublab <66892031+igithublab@users.noreply.github.com>
2021-01-31 10:47:04 +0800

    Create customize-feeds-lean.sh

---
+++ scripts/customize-feeds-lean.sh
@@ -0,0 +1,33 @@
+cd /workdir
+mkdir -p /workdir/openwrt/package/lean
+
+# Add luci-app-ssr-plus
+cd /workdir/openwrt/package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+cd /workdir/openwrt
+
+# Clone community packages to package/community
+mkdir package/community
+cd /workdir/openwrt/package/community
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
+# Add gotop
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gotop
+
+# Add smartdns
+svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
+
+# Fix mt76 wireless driver
+#cd  /workdir/openwrt/package/kernel/mt76
+#rm -f Makefile
+#wget https://raw.githubusercontent.com/openwrt/openwrt/master/package/kernel/mt76/Makefile
+#sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
